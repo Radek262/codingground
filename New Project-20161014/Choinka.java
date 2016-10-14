bcd202012b4da9f8d837525afb9c165d9dbab26b
Radek262 <rad.dunin@gmail.com>
2016-10-14 13:15:25 +0200

    Created by Coding Ground Client

---
+++ New Project-20161014/Choinka.java
@@ -0,0 +1,15 @@
+/*
+ Choinka
+ author: Radoslaw DW
+    Program wyswietla choinke
+*/
+
+public class Choinka{
+
+     public static void main(String []args){
+    for (int i=0;i<=6;i++)
+    {
+    System.out.print("*");
+    }
+     }
+}
