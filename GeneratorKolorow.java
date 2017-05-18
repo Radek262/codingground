79d81f31fcce6649eadb0f0dc1397b29c4d0eaa4
Radek262 <rad.dunin@gmail.com>
2017-05-18 15:59:25 +0200

    Add files via upload

--- GeneratorKolorow.java
+++ GeneratorKolorow.java
@@ -1,106 +1,103 @@
 /*
- * Nazwa: GeneratorKolorow.java
-   
-Autor: Radosław Dunin-Wąsowicz  (c) 2016
-   
-Album: 192663
-   Grupa: KrDZIs2012
-   
-Opis: 
-   Program przedstawia generatow kolorow w systemie szesnastkowym, wartosci ustawia sie za pomoca suwakow.
-   Wykorzystano elementy graficznego interfejsu oraz proste metody obliczeniowe.
-   
-Kompilacja: javac GeneratorKolorow.java
-    
-Uruchomienie: java GeneratorKolorow
- */
-
+* Nazwa: GeneratorKolorow.java
+* Autor: Tomasz Reczynski (c) 2016
+* Album: 194496
+* Grupa: KrDZIs2014
+*
+* Opis: Program ktory generuje kolor na podstawie R G B
+*
+*
+* Kompilacja: javac GeneratorKolorow.java
+* Uruchomienie: java GeneratorKolorow
+*
+*/
 
 import javax.swing.*;
-import java.awt.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
-public class GeneratorKolorow extends JFrame {
-	private JSlider slider1,slider2,slider3;
-	private JLabel code,sliders,number;
-	private JPanel body,footer;
-	
-	public GeneratorKolorow()
-	{
-		super("Generator kolorow");
-		setSize(400,300);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
-		//deklaracja komponentow
-		slider1 = new JSlider(JSlider.HORIZONTAL,0,250,0);
-		slider2 = new JSlider(JSlider.HORIZONTAL,0,250,0);
-		slider3 = new JSlider(JSlider.HORIZONTAL,0,250,0);
-		//ustawienie odleglosci zmiany wartosci
-		slider1.setMajorTickSpacing(50);
-		slider2.setMajorTickSpacing(50);
-		slider3.setMajorTickSpacing(50);
-		
-		//ustawienie przestrzeni na podzialce
-		slider1.setMinorTickSpacing(5);
-		slider2.setMinorTickSpacing(5);
-		slider3.setMinorTickSpacing(5);
+import java.awt.*;
 
-		//ustawienie widocznosci suwaka
-		slider1.setPaintTicks(true);
-		slider2.setPaintTicks(true);
-		slider3.setPaintTicks(true);
-		
-		//ustawienie widocznosci podzialki liniowej
-		slider1.setPaintLabels(true);
-		slider2.setPaintLabels(true);
-		slider3.setPaintLabels(true);
-		//zdefiniowanie etykiet
-		code = new JLabel("Kod koloru");
-		sliders = new JLabel("Suwaki R-G-B (0-250)");
-		number = new JLabel();
+public class GeneratorKolorow extends JFrame implements ChangeListener{
 
-		//zdefinowoanie "pojemnika,ciala" 
-		body = new JPanel();
-		footer = new JPanel();
-		
-		//menadzer rozkladu
-		add(sliders,BorderLayout.NORTH);
-		add(body,BorderLayout.CENTER);
-		body.setLayout(new GridLayout(5,1,2,2));
-		//dodanie suwakow
-		body.add(slider1);
-		body.add(slider2);
-		body.add(slider3);
-		body.add(code);
-		footer.add(number);
-		body.add(footer);
-	
+    private JLabel kodKoloru; // etykieta odpowiadajaca za wyswietlanie kodu koloru
+    private JLabel kod;
+    private JSlider sCzerwony, sZielony, sNiebieski; // Suwaki odpowiadajace za kolory
+    private JPanel panelKoloru; // Panel odpowiadajacy za wyswietlanie koloru
+    private int czerwony;
+    private int zielony;
+    private int niebieski;
 
-		
-		//zdefinowanie obiektu zmiany 
-		Operating change = new Operating();
-		slider1.addChangeListener(change);
-		slider2.addChangeListener(change);
-		slider3.addChangeListener(change);
-	}
+    public GeneratorKolorow(){
 
-	public class Operating implements ChangeListener
-	{
-		//obsluga zdarzen
-		public void stateChanged(ChangeEvent change) {
-			//wczytanie wartosci z suwakow
-			int value1 = slider1.getValue();
-			int value2 = slider2.getValue();
-			int value3 = slider3.getValue();
-			int sum = value1+value2+value3;
-			//konwersja na szesnastkowy i wydrukowanie
-			number.setText(Integer.toHexString(sum));
-		}
-	}
-	public static void main(String[] args)
-	{
-		//ustawienie widocznosci okna
-		GeneratorKolorow window = new GeneratorKolorow();
-		window.setVisible(true);
-	}
+        super("Generator Kolorow");
+        setSize(300,500);
+
+        sCzerwony = new JSlider(0,255,0); // tworzy nowy suwak
+        sCzerwony.setMajorTickSpacing(50); // wyznacza co ile maja byc ustawione glowne znaczniki w podzia�ce
+        sCzerwony.setMinorTickSpacing(5); // wyznacza co ile maja byc ustawione mniejsze znaczniki w podzialce
+        sCzerwony.setPaintTicks(true); // wartosc true oznacza ze znaczniki sa widoczne
+        sCzerwony.setPaintLabels(false); // wartosc true oznacza ze wartosci dla glownych znacznikow sa widoczne
+        sCzerwony.addChangeListener(this);
+
+
+        sZielony = new JSlider(0,255,0);
+        sZielony.setMajorTickSpacing(50);
+        sZielony.setMinorTickSpacing(5);
+        sZielony.setPaintTicks(true);
+        sZielony.setPaintLabels(true);
+        sZielony.addChangeListener(this);
+
+        sNiebieski = new JSlider(0,255,0);
+        sNiebieski.setMajorTickSpacing(50);
+        sNiebieski.setMinorTickSpacing(5);
+        sNiebieski.setPaintTicks(true);
+        sNiebieski.setPaintLabels(true);
+        sNiebieski.addChangeListener(this);
+
+
+        panelKoloru = new JPanel(); // panel odpowiadajcy za wyswietlanie koloru
+        panelKoloru.setBackground(Color.BLACK);
+        JPanel panel1 = new JPanel(); // panel odpowiadajacy za suwaki oraz etykiety
+        panel1.setLayout(new GridLayout(7,1));
+        JLabel nRGB = new JLabel("Suwaki R-G-B (0-255)");
+        nRGB.setHorizontalAlignment(JLabel.CENTER);
+        panel1.add(nRGB);
+        panel1.add(sCzerwony);
+        panel1.add(sZielony);
+        panel1.add(sNiebieski);
+        kod = new JLabel("Kod Koloru: ");
+        kod.setHorizontalAlignment(JLabel.CENTER);
+        kodKoloru = new JLabel();
+        kodKoloru.setHorizontalAlignment(JLabel.CENTER);
+        panel1.add(kod);
+        panel1.add(kodKoloru);
+        add(panel1, BorderLayout.NORTH);
+        add(panelKoloru, BorderLayout.CENTER);
+
+
+    }
+
+    public void stateChanged(ChangeEvent e) {
+
+        czerwony = sCzerwony.getValue(); // getValue pobiera wartosci typu calkowitego z suwakow i przypisuje do zmiennej
+        zielony = sZielony.getValue();
+        niebieski = sNiebieski.getValue();
+        Color kolor = new Color(czerwony, zielony, niebieski);
+        String kolorSzesnastkowo = Integer.toHexString(kolor.getRGB() & 0xffffff); //ge
+        kodKoloru.setText("#"+kolorSzesnastkowo); // wyswietla kod koloru
+        panelKoloru.setBackground(kolor); // zmienia t�o na wygenerowany przez suwaki kolor
+    }
+
+
+    public static void main(String [] args){
+        GeneratorKolorow kolory = new GeneratorKolorow();
+        kolory.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        kolory.setVisible(true);
+        kolory.setResizable(false);
+        kolory.setLocationRelativeTo(null);
+    }
+
+
+
 }
+
